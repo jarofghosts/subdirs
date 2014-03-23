f44539b683231601b04d1921011df63fb48e21bd
jesse keane <me@jessekeane.me>
2014-03-23 01:59:14 -0700

    add tests

---
+++ test/subdir.test.js
@@ -0,0 +1,35 @@
+var subdirs = require('../')
+
+var test = require('tape')
+  , path = require('path')
+
+test('finds all the subdirs!', function(t) {
+  t.plan(2)
+
+  subdirs(__dirname, function(err, dirs) {
+    t.ok(!err, 'no error')
+
+    var expected = [
+        f('a')
+      , f('b')
+      , f('b/c')
+      , f('d')
+      , f('.e')
+    ]
+
+    t.deepEqual(expected.sort(), dirs.sort(), 'finds all subdirs')
+  })
+})
+
+test('returns empty array if no subdirs', function(t) {
+  t.plan(2)
+
+  subdirs(f('a'), function(err, dirs) {
+    t.ok(!err, 'not an error')
+    t.deepEqual([], dirs)
+  })
+})
+
+function f(dir) {
+  return path.join(__dirname, dir)
+}
